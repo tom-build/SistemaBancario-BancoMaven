4c56118676c10b756628bbbdcc992810f24d0f5a
tomzudo <araujoerivelton324@gmail.com>
2026-05-09 18:53:30 -0300

    test: testes do arquivo ContaPoupança aprovados

--- src/test/java/com/banco/model/ContaPoupancaTest.java
+++ src/test/java/com/banco/model/ContaPoupancaTest.java
@@ -39,4 +39,96 @@ import static org.junit.jupiter.api.Assertions.*;
         assertThrows(IllegalArgumentException.class, () -> conta.sacar(-50));
     }
 
+    
+    @Test
+    void deveDepositarNormalmente() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        conta.depositar(50);
+
+        assertEquals(150, conta.getSaldo());
+    }
+
+    @Test
+    void naoDeveDepositarValorNegativo() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        assertThrows(IllegalArgumentException.class,
+            () -> conta.depositar(-50));
+    }
+
+    @Test
+    void deveFormatarONomeAutomaticamente() {
+        ContaPoupanca conta = new ContaPoupanca("   TOM   ", 100);
+
+        assertEquals("tom", conta.getNome());
+    }
+
+    @Test
+    void deveAlterarONomeCorretamente() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        conta.setNome("   João   ");
+
+        assertEquals("joão", conta.getNome());
+    }
+
+    @Test
+    void naoDevePermitirNomeVazioAoAlterar() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        assertThrows(IllegalArgumentException.class,
+            () -> conta.setNome(""));
+    }
+
+    @Test
+    void naoDevePermitirNomeNullAoAlterar() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        assertThrows(IllegalArgumentException.class,
+            () -> conta.setNome(null));
+    }
+
+    @Test
+    void deveAlterarSaldoCorretamente() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        conta.setSaldo(500);
+
+        assertEquals(500, conta.getSaldo());
+    }
+
+    @Test
+    void naoDevePermitirSaldoNegativoAoAlterar() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        assertThrows(IllegalArgumentException.class,
+            () -> conta.setSaldo(-10));
+    }
+
+    @Test
+    void deveDefinirIdCorretamente() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        conta.setId(1);
+
+        assertEquals(1, conta.getId());
+    }
+
+    @Test
+    void deveRetornarTipoCorretamente() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        assertEquals("poupanca", conta.getTipo());
+    }
+
+    @Test
+    void deveRetornarToStringCorretamente() {
+        ContaPoupanca conta = new ContaPoupanca("Tom", 100);
+
+        String esperado = "poupanca | Nome: tom | Saldo: 100.0";
+
+        assertEquals(esperado, conta.toString());
+    }
+
 }
