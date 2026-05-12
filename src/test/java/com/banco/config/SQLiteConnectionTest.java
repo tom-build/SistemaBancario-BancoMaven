5948271f75450623f046b9b9302e08bdf9967121
tomzudo <araujoerivelton324@gmail.com>
2026-05-11 22:59:41 -0300

    refactor: mudança no codigo do método conectar, permitindo que a classe de teste conecte com o próprio banco, sem mudar dados sensíveis

--- src/test/java/com/banco/config/SQLiteConnectionTest.java
+++ src/test/java/com/banco/config/SQLiteConnectionTest.java
@@ -1,5 +1,26 @@
 package com.banco.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SQLiteConnectionTest {
-    
+
+    private static final String URL =
+        "jdbc:sqlite:bancoTest.db";
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager
+                .getConnection(URL);
+
+        } catch (SQLException e) {
+
+            throw new RuntimeException(
+                "Erro ao conectar no banco de teste",
+                e
+            );
+        }
+    }
 }
