b11b3eebdd89f72ec2e13dd16d3531c24c455cff
tomzudo <araujoerivelton324@gmail.com>
2026-05-15 10:07:42 -0300

    test: teste de validação de inteiros e ToString adicionados no ContaBancariaTest

--- src/test/java/com/banco/model/ContaBancariaTest.java
+++ src/test/java/com/banco/model/ContaBancariaTest.java
@@ -8,4 +8,18 @@ class ContaBancariaTest {
     void deveFormatarString() {
         assertEquals("tom", ContaBancaria.formatar("TOM"));
     }
+
+    @Test
+    void deveValidarInt() {
+        assertThrows(IllegalArgumentException.class, () -> ContaBancaria.validarInt(-1));
+    }
+
+    @Test
+    void deveRetornarToStringCorretamente() {
+        ContaCorrente conta = new ContaCorrente("Tom", 100);
+
+        String esperado = "corrente | Nome: tom | Saldo: 100.0";
+
+        assertEquals(esperado, conta.toString());
+    }
 }
