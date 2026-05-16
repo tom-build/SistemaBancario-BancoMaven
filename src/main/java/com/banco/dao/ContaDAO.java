887b60dbc132803a5609872dd5c7decc77bb4855
tomzudo <araujoerivelton324@gmail.com>
2026-05-15 21:51:50 -0300

    test: metodo de criar tabela validado no arquivo DatabaseTest

--- src/main/java/com/banco/dao/ContaDAO.java
+++ src/main/java/com/banco/dao/ContaDAO.java
@@ -17,10 +17,7 @@ public class ContaDAO {
 
         String sql = "INSERT INTO contas (nome, saldo, tipo) VALUES (?, ?, ?)";
 
-        try (
-                PreparedStatement ps = conn.prepareStatement(
-                        sql,
-                        Statement.RETURN_GENERATED_KEYS)) {
+        try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
             ps.setString(1, conta.getNome());
             ps.setDouble(2, conta.getSaldo());
@@ -35,41 +32,31 @@ public class ContaDAO {
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(
-                    "Erro ao salvar conta",
-                    e);
+            throw new RuntimeException("Erro ao salvar conta", e);
         }
     }
 
     public List<ContaBancaria> listar() {
 
         List<ContaBancaria> lista = new ArrayList<>();
-
+        
         String sql = "SELECT * FROM contas";
 
-        try (
-                PreparedStatement ps = conn.prepareStatement(sql);
-
+        try (PreparedStatement ps = conn.prepareStatement(sql);
                 ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
-
                 String tipo = rs.getString("tipo");
 
                 ContaBancaria conta;
 
-                if ("corrente"
-                        .equalsIgnoreCase(tipo)) {
-
+                if ("corrente".equalsIgnoreCase(tipo)) {
                     conta = new ContaCorrente(
                             rs.getString("nome"),
                             rs.getDouble("saldo"));
 
                 } else {
-
-                    conta = new ContaPoupanca(
-                            rs.getString("nome"),
-                            rs.getDouble("saldo"));
+                    conta = new ContaPoupanca(rs.getString("nome"), rs.getDouble("saldo"));
                 }
 
                 conta.setId(rs.getInt("id"));
@@ -78,22 +65,17 @@ public class ContaDAO {
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(
-                    "Erro ao listar contas",
-                    e);
+            throw new RuntimeException("Erro ao listar contas", e);
         }
 
         return lista;
     }
 
-    public void atualizarSaldo(
-            int id,
-            double novoSaldo) {
+    public void atualizarSaldo(int id, double novoSaldo) {
 
         String sql = "UPDATE contas SET saldo = ? WHERE id = ?";
 
-        try (
-                PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setDouble(1, novoSaldo);
             stmt.setInt(2, id);
@@ -101,52 +83,34 @@ public class ContaDAO {
             int linhas = stmt.executeUpdate();
 
             if (linhas == 0) {
-
-                throw new RuntimeException(
-                        "Conta não encontrada para atualização.");
+                throw new RuntimeException("Conta não encontrada para atualização.");
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(
-                    "Erro ao atualizar saldo",
-                    e);
+            throw new RuntimeException("Erro ao atualizar saldo", e);
         }
     }
 
     public ContaBancaria buscarPorId(int id) {
-
         String sql = "SELECT * FROM contas WHERE id = ?";
 
-        try (
-                PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, id);
 
-            try (
-                    ResultSet rs = stmt.executeQuery()) {
-
+            try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
-
                     String tipo = rs.getString("tipo");
-
                     String nome = rs.getString("nome");
-
                     double saldo = rs.getDouble("saldo");
 
                     ContaBancaria conta;
 
-                    if ("corrente"
-                            .equalsIgnoreCase(tipo)) {
-
-                        conta = new ContaCorrente(
-                                nome,
-                                saldo);
+                    if ("corrente".equalsIgnoreCase(tipo)) {
+                        conta = new ContaCorrente(nome, saldo);
 
                     } else {
-
-                        conta = new ContaPoupanca(
-                                nome,
-                                saldo);
+                        conta = new ContaPoupanca(nome, saldo);
                     }
 
                     conta.setId(id);
@@ -156,9 +120,7 @@ public class ContaDAO {
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(
-                    "Erro ao buscar conta",
-                    e);
+            throw new RuntimeException("Erro ao buscar conta", e);
         }
 
         return null;
@@ -168,23 +130,18 @@ public class ContaDAO {
 
         String sql = "DELETE FROM contas WHERE id = ?";
 
-        try (
-                PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, id);
 
             int linhasAfetadas = stmt.executeUpdate();
 
             if (linhasAfetadas == 0) {
-
-                throw new RuntimeException(
-                        "Conta não encontrada.");
+                throw new RuntimeException("Conta não encontrada.");
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(
-                    "Erro ao excluir conta",
-                    e);
+            throw new RuntimeException("Erro ao excluir conta", e);
         }
     }
 }
--- src/test/java/com/banco/config/DatabaseTest.java
+++ src/test/java/com/banco/config/DatabaseTest.java
@@ -1,5 +1,23 @@
 package com.banco.config;
 
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DatabaseTest {
-    
+    @Test
+    void deveCriarTabelaContas() throws Exception {
+
+        Database.inicializar();
+        try (Connection conn = SQLiteConnection.conectar()) {
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery("SELECT name FROM sqlite_master " + "WHERE type = 'table' AND name = 'contas' ");
+            assertTrue(rs.next());
+        }
+    }
 }
